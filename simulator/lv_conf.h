dc69acbd34a394f5e284fc2ba7fa3dbb0df90225
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-24 13:15:45 +0800

    simulator: add LVGL configuration for macOS preview

---
+++ simulator/lv_conf.h
@@ -0,0 +1,20 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH 16
+#define LV_USE_OS LV_OS_NONE
+#define LV_USE_LOG 1
+#define LV_USE_ASSERT_NULL 1
+#define LV_USE_ASSERT_MALLOC 1
+
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_28 1
+#define LV_FONT_DEFAULT &lv_font_montserrat_14
+
+#define LV_USE_LABEL 1
+#define LV_USE_SLIDER 1
+#define LV_USE_SWITCH 1
+
+#endif
