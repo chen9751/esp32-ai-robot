9e15df9387bfbb00aab74669012ee9a155a65684
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-24 12:55:34 +0800

    ui: add first 320x172 LVGL prototype

--- components/ui/include/ui_manager.h
+++ components/ui/include/ui_manager.h
@@ -1,3 +1,11 @@
 #pragma once
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 void ui_init(void);
+void ui_show_home(void);
+void ui_show_functions(void);
+void ui_show_settings(void);
+#ifdef __cplusplus
+}
+#endif
