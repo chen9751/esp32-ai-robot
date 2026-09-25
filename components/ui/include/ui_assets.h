ea8faff9b0ca1d55e4922af6b1b9a3f7b7822c12
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-25 22:14:07 +0800

    ui: define homepage image assets

--- components/ui/include/ui_assets.h
+++ components/ui/include/ui_assets.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "lvgl.h"
 
-extern const lv_image_dsc_t ui_icon_remote;
-extern const lv_image_dsc_t ui_label_remote;
-extern const lv_image_dsc_t ui_icon_music;
-extern const lv_image_dsc_t ui_label_music;
-extern const lv_image_dsc_t ui_icon_light;
-extern const lv_image_dsc_t ui_label_light;
-extern const lv_image_dsc_t ui_icon_devices;
-extern const lv_image_dsc_t ui_label_devices;
-extern const lv_image_dsc_t ui_icon_alarm;
-extern const lv_image_dsc_t ui_label_alarm;
-extern const lv_image_dsc_t ui_icon_settings;
-extern const lv_image_dsc_t ui_label_settings;
+void ui_assets_init(void);
+
+extern lv_image_dsc_t ui_icon_remote;
+extern lv_image_dsc_t ui_label_remote;
+extern lv_image_dsc_t ui_icon_music;
+extern lv_image_dsc_t ui_label_music;
+extern lv_image_dsc_t ui_icon_light;
+extern lv_image_dsc_t ui_label_light;
+extern lv_image_dsc_t ui_icon_devices;
+extern lv_image_dsc_t ui_label_devices;
+extern lv_image_dsc_t ui_icon_alarm;
+extern lv_image_dsc_t ui_label_alarm;
+extern lv_image_dsc_t ui_icon_settings;
+extern lv_image_dsc_t ui_label_settings;
