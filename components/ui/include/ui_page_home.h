d110a68b54db0b232438073262f5fbeb0a9dc8bb
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-26 06:38:37 +0800

    ui: expose home builder for interactive transitions

--- components/ui/include/ui_page_home.h
+++ components/ui/include/ui_page_home.h
@@ -4,6 +4,12 @@
 
 typedef void (*ui_page_activity_cb_t)(void *user_data);
 
+lv_obj_t *ui_page_home_build(lv_obj_t *parent,
+                             ui_menu_action_cb_t action_cb,
+                             void *action_user_data,
+                             ui_page_activity_cb_t activity_cb,
+                             void *activity_user_data);
+
 void ui_page_home_show(ui_menu_action_cb_t action_cb,
                        void *action_user_data,
                        ui_page_activity_cb_t activity_cb,
