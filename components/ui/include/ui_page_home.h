7036d9dd0dc11c83b58354ec043df3c6f9224a44
chen9751 <99879461+chen9751@users.noreply.github.com>
2026-09-25 23:53:04 +0800

    ui: add home page interface

---
+++ components/ui/include/ui_page_home.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "ui_manager.h"
+
+typedef void (*ui_page_activity_cb_t)(void *user_data);
+
+void ui_page_home_show(ui_menu_action_cb_t action_cb,
+                       void *action_user_data,
+                       ui_page_activity_cb_t activity_cb,
+                       void *activity_user_data);
